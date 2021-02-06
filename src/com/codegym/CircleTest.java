68fd2346977f816b70600dd6c40b07466f914d4c
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-02-06 09:17:07 +0700

    first commit

---
+++ src/com/codegym/CircleTest.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+    }
+}
---
+++ src/com/codegym/ShapeTest.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+public class ShapeTest {
+
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
